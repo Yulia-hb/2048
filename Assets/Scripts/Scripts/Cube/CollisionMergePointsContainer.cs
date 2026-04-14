4a0e39a1d3d5c17673011c8c80f4671a55f472d3
Yulia-hb <yuliabalanik@outlook.com>
2026-04-14 20:53:18 +0300

    [+] add gamemabager, camerashake, particle system, add skybox

--- Assets/Scripts/Scripts/Cube/CollisionMergePointsContainer.cs
+++ Assets/Scripts/Scripts/Cube/CollisionMergePointsContainer.cs
@@ -7,11 +7,14 @@ namespace ChainCube.Scripts.Cube
     {
         private PointsContainer _score;
         private PointsContainerCollisionDetector _detector;
+        [SerializeField] private GameObject _mergeEffect;
+        private ScoreManager _scoreManager;
 
         private void Start()
         {
             _score = GetComponent<PointsContainer>();
             _detector = GetComponent<PointsContainerCollisionDetector>();
+            _scoreManager = FindObjectOfType<ScoreManager>(); // норм для зараз
             Subscribe();
         }
 
@@ -20,7 +23,24 @@ namespace ChainCube.Scripts.Cube
             if (col.points == _score.points)
             {
                 _score.points *= 2;
+                _scoreManager?.AddScore(_score.points);
 
+                // 💥 PARTICLE
+                if (_mergeEffect != null)
+                {
+                    var effect = Instantiate(_mergeEffect, transform.position, Quaternion.identity);
+                    Destroy(effect, 1f);
+                }
+
+                // 🎥 CAMERA SHAKE
+                var cam = Camera.main.GetComponent<CameraShake>();
+
+                if (cam != null)
+                {
+                    StartCoroutine(cam.Shake(0.1f, 0.1f));
+                }
+
+                // ❗ Destroy
                 if (col != null && col.gameObject.scene.IsValid())
                 {
                     Destroy(col.gameObject);
